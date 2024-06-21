111da013b9d684811aaff6bd5b478cb5e210ae8d
ty.meekel <ty.meekel@gmail.com>
2024-06-21 00:31:34 +0000

    Test

--- idx-template.nix
+++ idx-template.nix
@@ -1,11 +1,3 @@
 { pkgs, ... }: {
-    bootstrap = ''
-    # Create new Gleam project, move contents to root, delete base folder
-    gleam new temp
-    mv ./temp/* .
-    rm -r temp
-
-    # Delete README file
-    rm README.md
-    '';
+    
 }
